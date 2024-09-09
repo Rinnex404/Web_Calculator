2b6e483934b23dbaeae5b9cc769710b741a97693
Rinnex404 <138128240+Rinnex404@users.noreply.github.com>
2024-09-09 22:29:24 +0800

    add 4 function to add,subtract,multiplt,divide number

---
+++ script.js
@@ -0,0 +1,20 @@
+function getAdd(num1, num2){
+    let result = num1 + num2;
+    return result;
+}
+
+function getSubtract(num1, num2){
+    let result = num1 - num2;
+    return result;
+}
+
+function getMultiply(num1, num2){
+    let result = num1 * num2;
+    return result;
+}
+
+function getDivide(num1, num2){
+    let result = num1 / num2;
+    return result;
+}
+

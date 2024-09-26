8ef1f130f7ee7cee0b7b24ab2749a7050ff909c6
Rinnex404 <138128240+Rinnex404@users.noreply.github.com>
2024-09-26 22:53:01 +0800

    Implement calculator button functionality&display
    - Capture value from number and operator buttons
    - Display result when 'equal' button is pressed
    - Automatically calculate and show result if a third number is pressed after an operator

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function getDivide(num1, num2){
     return result;
 }
 
-let firstnum, calculation, secnum, total;
+let firstnum, calculation, secnum, total, ifNumIsPressed, ifSubmitIsPressed, currentnum, displayvalue;
 
 function operator(fnum, calc, snum){
     switch(calc){
@@ -41,15 +41,71 @@ function operator(fnum, calc, snum){
     return total;
 }
 
-const btn = document.querySelectorAll(".calcbutton");
-btn.forEach((btn) => {
-    btn.addEventListener("click", ()=>{
-    let num = btn.value;
-    display(num);
+function showResult(){
+    let result = operator(firstnum, calculation, secnum);
+    //console.log(firstnum + "" + calculation + "" + secnum + "=" + result);
+    ifNumIsPressed = false;
+    ifSubmitIsPressed = true;
+    numDis.value = result;
+}
+
+const btn = document.querySelectorAll(".calcbutton");   //button for (1-9)
+const calc_buttons = document.querySelectorAll(".operator"); //button for (/*-+)
+const submit_button = document.querySelector("#submit"); //button for (=)
+let numDis = document.querySelector(".display"); //input display
+
+btn.forEach((btns) => {
+    btns.addEventListener("click", ()=>{
+        if(ifNumIsPressed === false){
+            numDis.value = "";              //reset display number if second number is entered
+        }
+
+        currentnum = btns.value;
+        ifNumIsPressed = true;
+        display(currentnum);
 });
 });
 
 function display(num){
-    let numDis = document.querySelector(".display");
-    numDis.value = num;
+    if(ifSubmitIsPressed === true){     //if user press a new batch number instead of new operator it will reset all value
+        firstnum = null;                
+        secnum = null;
+        calculation = null;
+        ifSubmitIsPressed = false;
+    }
+
+    numDis.value += num;
 }
+
+
+calc_buttons.forEach((calc_button) => {
+    calc_button.addEventListener("click", ()=>{
+
+        if(ifNumIsPressed === true && firstnum != null){    //check if first and second number is entered,
+            secnum = Number(numDis.value);                  //and will direct show result if user press operator button instead of equal button
+            showResult();
+        }
+
+        calculation = calc_button.value;
+        ifNumIsPressed = false;
+        ifSubmitIsPressed = false;
+        firstnum = Number(numDis.value);
+
+});
+});
+
+
+submit_button.addEventListener("click", ()=>{
+    if(ifNumIsPressed === false){   //clear display value
+        numDis.value = "";
+    }
+
+    secnum = Number(numDis.value);
+
+    if(firstnum != null && calculation != null && secnum != null){  //if both number, operator and equal is pressed, show result
+        showResult();
+    }
+});
+
+
+
